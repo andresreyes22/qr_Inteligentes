fd9d8d7846ffc7ab7e4d69cf83155cb378310337
andres.reyes <andresm.reyes@oncredit.com.co>
2024-12-18 22:22:25 -0500

    cambios en llamados maze main

--- detectar_figura/server3.py
+++ detectar_figura/server3.py
@@ -7,7 +7,7 @@ import threading
 app = Flask(__name__)
 
 # Inicializar variables globales para la cámara y la última imagen capturada
-camera = cv2.VideoCapture("http://192.168.104.25:4747/video")  # Abrir la cámara
+camera = cv2.VideoCapture("http://192.168.1.8:4747/video")  # Abrir la cámara
 #camera = cv2.VideoCapture(0)
 latest_frame = None
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
